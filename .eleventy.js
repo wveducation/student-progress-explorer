10c1cc1b2fac46f32e8b1afec171957d16290061
Aaron Silber <aaronlsilber@gmail.com>
2021-08-17 15:16:35 -0400

    build sys config

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,51 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const util = require('util');
+const svgContents = require("eleventy-plugin-svg-contents");
+const Image = require("@11ty/eleventy-img");
+
+function imageShortcode({src, alt, cls, styleName}) {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  if (styleName === undefined) {
+    throw new Error(`Missing \`styleName\` on myImage from: ${src}`);
+  }
+
+  let styles = {
+    medium_half: {
+      sizes: "(max-width: 767px) 100vw, 600px"
+    }
+  }
+
+  let options = {
+    widths: [640, 768, 1024],
+    formats: ['webp', 'jpeg'],
+    urlPath: "/static/img/",
+    outputDir: "./_site/static/img/"
+  };
+
+  Image(src, options);
+
+  let sizes = styles[styleName].sizes;
+
+  let imageAttributes = {
+    class: cls,
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+  // get metadata even the images are not fully generated
+  metadata = Image.statsSync(src, options);
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(svgContents); 
+
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
@@ -21,6 +64,47 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
     );
+  });
+
+  eleventyConfig.addFilter("sortByName", (items) => {
+    let data = [...items];
+    return data.sort((a, b) => { b.data.name.localeCompare(a.data.name) });
+  })
+
+  eleventyConfig.addFilter("sortResultsDirectory", (results) => {
+    // Using sort for multiple columns
+    // https://dev.to/markbdsouza/js-sort-an-array-of-objects-on-multiple-columns-keys-2bj1
+
+    // avoids mutating results collection
+    let data = [...results];
+    // sorts by grade, then subject, then area.
+    return data.sort((a, b) => (a.data.subject.localeCompare(b.data.subject) || a.data.grade - b.data.grade || a.data.area.localeCompare(b.data.area)) );
+  });
+
+  eleventyConfig.addShortcode("image", imageShortcode);
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  
+  eleventyConfig.addShortcode("getItemName", (collection, id) => {
+
+    let items = collection.filter( (item) => {
+      if (item.data.id == id ) {
+        return item;
+      }
+    });
+    
+    return items[0].data.name;
+  });
+
+  eleventyConfig.addShortcode("getSubjectField", (collection, id, fieldName) => {
+
+    let items = collection.filter( (item) => {
+      if (item.data.id == id ) {
+        return item;
+      }
+    });
+    
+    return items[0].data[fieldName];
   });
 
   eleventyConfig.addShortcode("subjectTree", (results, subjects, grades, areas) => {
@@ -85,9 +169,9 @@ module.exports = function(eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
+    "./src/static/fonts/icons/fonts/*": "./static/fonts/icons/fonts/",
+    "./src/static/fonts/icons/style.css": "./static/fonts/icons/style.css",
   });
 
   // Copy Image Folder to /_site
